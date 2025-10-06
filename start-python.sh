442c3c87c356b69d119d1285bb663dcfa11b0290
Ulul Azmi <85116521+ululazmi18@users.noreply.github.com>
2025-10-06 17:12:04 +0700

    Update start-python.sh

--- start-python.sh
+++ start-python.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$BASEDIR/python-service"
+# Aktifkan virtual environment jika ada
+if [ -d "venv" ]; then
+    . "venv/bin/activate"
+else
+    echo "⚠️  Virtualenv tidak ditemukan di $BASEDIR/python-service/venv. Jalankan 'bash setup.sh' terlebih dahulu."
+fi
 python app.py
